3755b925aa07934371425ea90ee13034e45a5fcb
Taras Volchenko <exeq00@gmail.com>
2023-11-15 15:04:19 +0400

    Refactoring: moved user's code to controller

---
+++ src/controllers/user.ballance.controller.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const models = require("../models");
+
+let UserBalanceController = {
+    update: async (req, res) => {
+        const userId = req.params.userId;
+        const amount = parseInt(req.body.amount);
+        if (isNaN(amount)) {
+            return res.status(400).json({ error: 'Invalid amount' });
+        }
+            try {
+            let result = await models.User.update(
+                { balance: Sequelize.literal(`balance - ${amount}`) },
+                { where: { id: userId } }
+                )
+                if (result[0] === 1) {
+                    console.log('Balance successfully updated.');
+                } else {
+                    return res.status(404).json({ error: 'User not found' });
+                }
+                res.json({ success: true });
+        } catch (error) {
+            if (error.name === 'SequelizeUniqueConstraintError') {
+                res.send({success: false, message: "Insufficient balance"})
+            } else {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        }
+    }
+};
+
+module.exports = UserBalanceController
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,34 +1,7 @@
 const Router = require("express").Router;
-const Sequelize = require('sequelize');
-const models = require("../models");
 const userRoutes = Router();
+const UserBalanceController = require('../controllers/user.ballance.controller');
 
-userRoutes.post('/updateBalance/:userId', async (req, res) => {
-    const userId = req.params.userId;
-    const amount = parseInt(req.body.amount);
-  
-    if (isNaN(amount)) {
-      return res.rstatus(400).json({ error: 'Invalid amount' });
-    }
-  
-    try {
-      let result = await models.User.update(
-          { balance: Sequelize.literal(`balance - ${amount}`) },
-          { where: { id: userId } }
-        )
-      if (result[0] === 1) {
-          console.log('Balance successfully updated.');
-      } else {
-          return res.status(404).json({ error: 'User not found' });
-      }
-      res.json({ success: true });
-    } catch (error) {
-      if (error.name === 'SequelizeUniqueConstraintError') {
-        res.send({success: false, message: "Insufficient balance"})
-      } else {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
-    }
-});
+userRoutes.post('/updateBalance/:userId', UserBalanceController.update);
+
 module.exports = userRoutes
